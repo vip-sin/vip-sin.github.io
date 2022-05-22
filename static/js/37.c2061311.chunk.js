8c680687d660de18ab2d4e351a821a388d1a4cd7
vip-sin <vpnisvipin@gmail.com>
2022-05-22 22:03:27 +0100

    Cleared gh pages

--- static/js/37.c2061311.chunk.js
+++
@@ -1,1 +0,0 @@
-(this.webpackJsonplastArzi=this.webpackJsonplastArzi||[]).push([[37],{709:function(t,e,n){"use strict";n.r(e);var a=n(0),r=n.n(a),i=n(465);e.default=function(){return r.a.createElement(i.a,{md:"12",style:{marginTop:"70px",textAlign:"center"}},"Privacy")}}}]);
