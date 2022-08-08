bade6634fb844cc2994a0bcd8a364cffaa139547
vip-sin <vpnisvipin@gmail.com>
2022-08-09 00:46:31 +0100

    Latest code deployed

--- static/js/36.70a5bd5e.chunk.js
+++
@@ -1,1 +0,0 @@
-(this.webpackJsonplastArzi=this.webpackJsonplastArzi||[]).push([[36],{699:function(t,e,n){"use strict";n.r(e);var a=n(0),r=n.n(a),i=n(468);e.default=function(){return r.a.createElement(i.a,{md:"12",style:{marginTop:"70px",textAlign:"center"}},"Privacy")}}}]);
